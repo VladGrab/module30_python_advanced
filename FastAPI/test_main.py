be110b802a6672af296edea798891f516607efdf
vladislav <bull2099@yandex.ru>
2025-08-07 01:08:17 +0300

    0.0.5 test run CL/CD

--- FastAPI/test_main.py
+++ FastAPI/test_main.py
@@ -6,9 +6,11 @@ from main import app
 
 client = TestClient(app)
 
+
 @pytest.fixture
 def anyio_backend():
     return 'asyncio'
+
 
 # @pytest.fixture(scope="session")
 @pytest.mark.anyio
@@ -27,16 +29,16 @@ async def test_add_recipe():
             transport=ASGITransport(app=app), base_url="http://127.0.0.1:8000"
     ) as ac:
         response = await ac.post(
-            "/recipes/",
-             headers={"Content-Type": "application/json"},
-             json={
-                    "name": "test",
-                    "cooking_time": 40,
-                    "ingredients_list": "test, test_2",
-                    "description": "just test",
-                    "count_view": 0
-                }
-            )
+                    "/recipes/",
+                     headers={"Content-Type": "application/json"},
+                     json={
+                            "name": "test",
+                            "cooking_time": 40,
+                            "ingredients_list": "test, test_2",
+                            "description": "just test",
+                            "count_view": 0
+                        }
+                    )
         assert response.status_code == 200
         assert response.json() == {
                     "name": "test",
