1a480b79361ea0e8c7e7ff283d6e7e1a195374c5
vladislav <bull2099@yandex.ru>
2025-08-06 23:53:55 +0300

    0.0.3 test run CL/CD

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 #     await session.close()
 #     await engine.dispose()
 #
-print('big bang')
+print('big bang_02')
 
 @app.get('/recipes/', response_model=List[schemas.BaseRecipe])
 async def recipes() -> List[models.Recipe]:
