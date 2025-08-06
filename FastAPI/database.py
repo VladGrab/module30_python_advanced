bcbc342d377293eadda045d18c38c78533f9bf3a
vladislav <bull2099@yandex.ru>
2025-08-07 02:13:35 +0300

    0.0.5 test run CL/CD

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -5,8 +5,6 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite+aiosqlite:///./app.py.db"
 
 engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
-# expire_on_commit=False will prevent attributes from being expired
-# after commit.
 async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 session = async_session()
 Base = declarative_base()
