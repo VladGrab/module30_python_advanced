7060642275fe7c567c490945785659f4ba8d253c
vladislav <bull2099@yandex.ru>
2025-08-07 01:35:12 +0300

    0.0.5 test run CL/CD

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -25,8 +25,8 @@ async def recipes_info(recipe_id: int) -> List[models.Recipe]:
         records = res.scalars().all()
         count_view_get: int = (records[0].count_view)
         query_update_count_view = (update(models.Recipe).
-                                       where(models.Recipe.id == recipe_id).
-                                       values(count_view=count_view_get + 1))
+                                    where(models.Recipe.id == recipe_id).
+                                    values(count_view=count_view_get + 1))
         await session.execute(query_update_count_view)
     return records
 
