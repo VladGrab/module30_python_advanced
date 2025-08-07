57caed88ee7130e8393c0ede1dc015720e78296a
vladislav <bull2099@yandex.ru>
2025-08-07 23:09:57 +0300

    0.0.5 test run CL/CD

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import declarative_base
-
+# from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 # from database import Base
 Base = declarative_base()
 
