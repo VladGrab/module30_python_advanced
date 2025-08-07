36a6bf5e1400be61a8e3d20073e6f1df13aea980
vladislav <bull2099@yandex.ru>
2025-08-07 23:20:17 +0300

    0.0.5 test run CL/CD

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import declarative_base, Mapped
+from sqlalchemy.orm import declarative_base
 # from sqlalchemy.ext.declarative import declarative_base
 # from database import Base
 Base = declarative_base()
 
 class Recipe(Base):
     __tablename__ = 'Recipe'
-    id: Mapped[int] = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name: Mapped[str] = Column(String)
-    cooking_time: Mapped[int] = Column(Integer, default=0)
-    ingredients_list: Mapped[str] = Column(String)
-    description: Mapped[str] = Column(String)
-    count_view: Mapped[int] = Column(Integer, default=0)
+    id: Column[int] = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name: Column[str] = Column(String)
+    cooking_time: Column[int] = Column(Integer, default=0)
+    ingredients_list: Column[str] = Column(String)
+    description: Column[str] = Column(String)
+    count_view: Column[int] = Column(Integer, default=0)
