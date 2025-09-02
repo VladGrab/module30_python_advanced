d61f324b5fb160e77c3750dceb84904eec5af4fb
vladislav <bull2099@yandex.ru>
2025-09-02 22:05:01 +0300

    0.0.5 test run CL/CD

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
                                     async_sessionmaker, create_async_engine)
 
-
 DATABASE_URL = "sqlite+aiosqlite:///./app.py.db"
 
 engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
